48c75cb1913e330444daa9aae2845557ac883ced
Caio Hamc <caiohamc@MacBook-Pro-de-Caio.local>
2024-05-16 01:50:00 -0300

    Initial commit

---
+++ src/test/java/br/com/alchacos/demo/mongo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alchacos.demo.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
